9188800d52578a660b9e38d39a97f5660c53b8ba
Dimas Abreu <95443404+dimasabreu@users.noreply.github.com>
2023-06-13 09:31:50 -0300

    Update streamlit_app.py
    new item and partition

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,10 @@ st.title('My Parents New Healthy Diner')
 
 st.header('Breakfast Menu')
 
-st.text('OMega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.text('🥣 OMega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
    
